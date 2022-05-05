42a6d5c666425e5b7a09690e5ff8d7a916eb901d
Lofty | Brambles <prantikdas2004@gmail.com>
2022-05-05 20:17:05 +0530

    ➕ | Merges stylesheet, Adds main GUI

--- tab.js
+++ tab.js
@@ -1,13 +1,13 @@
 function openTab( event, tabName ) {
 	
-	const tabId = document.querySelectorAll( "[id^=main]" );
-	for ( let i = 0; i < tabId.length; i++ ) {
-		tabId[ i ].style.display = "none";
-	}
-
 	const dash = document.getElementsByClassName( "dash" );
 	for ( let i = 0; i < dash.length; i++ ) {
 		dash[ i ].className = dash[ i ].className.replace( " active", "" );
+	}
+	
+	const tabId = document.querySelectorAll( "[id^=main]" );
+	for ( let i = 0; i < tabId.length; i++ ) {
+		tabId[ i ].style.display = "none";
 	}
 
 	document.getElementById( tabName ).style.display = "block";
